1988d5e178ee39c3b475dcd0a292957122a8373f
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-09-22 13:50:51 +0100

    removed canvas plans, started adding items for testing purse css javascript version

--- js/main.js
+++ js/main.js
@@ -4,25 +4,17 @@
 
 // BASIC CONTROLS 
 // canvas
-const canvas = document.getElementById("gameArea");
-let ctx = canvas.getContext("2d");
-const drawImage = (url) => {
-  const image = new Image();
-  image.src = url;
-  image.onload = () => {
-     ctx.drawImage(image, 0, 0)
-  }
-}
 
-drawImage("./resources/images/sprite.jpg")
 
 // player character must exist.
 // sprite?
 // map must exist, floors and gaps, objects
-//canvas can draw?
 
+// player must fall, gravity
 // player character must move with input
 // left, right, jump
-//player must fall, gravity
+const sprite = document.querySelector(".character__sprite")
+
+
 
 // enemies must exist
