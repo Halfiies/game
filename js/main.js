73992680a0c59c17d9825e51f08abedaad42fbe4
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-09-23 14:35:45 +0100

    gravity working, merging branches

--- js/main.js
+++ js/main.js
@@ -11,34 +11,39 @@
 const sprite = document.querySelector(".character")
 const platform1 = document.querySelector(".platform__one")
 const platform2 = document.querySelector(".platform__one")
+const platOneDetails = platform1.getBoundingClientRect();
+console.log(platOneDetails)
 sprite.style.bottom = "30px"
 sprite.style.left ="0px"
+
 // player character must move with input
 
 // left, right, jump
 
 const whichkey = (logkey) => { 
-    if (logkey.code === 'ArrowUp' ){
+    if (logkey.code === 'ArrowUp' && parseInt(sprite.style.bottom)<=420){
      let heightString = sprite.style.bottom.replace('px', '')
      var height = parseInt(heightString, 10)
      sprite.style.bottom = `${height + 30}px`
     }
-    if (logkey.code === "ArrowLeft"){
+    if (logkey.code === "ArrowLeft" && parseInt(sprite.style.left)>= 5){
      let leftString = sprite.style.left.replace('px', '')
      var left = parseInt(leftString, 10)
      sprite.style.left = `${left - 10}px`
     }
-    if (logkey.code === "ArrowRight"){
+    if (logkey.code === "ArrowRight" && parseInt(sprite.style.left)<=460){
      let leftString = sprite.style.left.replace('px', '')
      var left = parseInt(leftString, 10)
      sprite.style.left = `${left + 10}px`
    } else {}
    };
-const gravityEffects = () => {
-   let heightString = sprite.style.bottom.replace('px', '')
-   var height = parseInt(heightString, 10)
-   sprite.style.bottom = `${height - 1}px`
+const gravityEffects = () => { 
+   if (parseInt(sprite.style.bottom)>= 1) {
+      let heightString = sprite.style.bottom.replace('px', '')
+      var height = parseInt(heightString, 10)
+      sprite.style.bottom = `${height - 1}px`
+   } else {}
 };  
-   document.addEventListener("keydown", whichkey);
-
+document.addEventListener("keydown", whichkey);
 let gameInterval = setInterval(gravityEffects, 50)
+
