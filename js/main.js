0a1e4506839818e7e381710f348c4cae49c943cf
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-09-23 15:10:33 +0100

    basic collision detection on one platform

--- js/main.js
+++ js/main.js
@@ -11,14 +11,26 @@
 const sprite = document.querySelector(".character")
 const platform1 = document.querySelector(".platform__one")
 const platform2 = document.querySelector(".platform__one")
-const platOneDetails = platform1.getBoundingClientRect();
-console.log(platOneDetails)
+
 sprite.style.bottom = "30px"
 sprite.style.left ="0px"
+
 
 // player character must move with input
 
 // left, right, jump
+const collisionDetect = () => {
+   const platOneDetails = platform1.getBoundingClientRect();
+   const platTwoDetails = platform2.getBoundingClientRect();
+   let spriteDetails = sprite.getBoundingClientRect();
+ if (spriteDetails.x <= platOneDetails.x + platOneDetails.width &&
+   spriteDetails.x + spriteDetails.width >= platOneDetails.x &&
+   spriteDetails.y <= platOneDetails.y + platOneDetails.height &&
+   spriteDetails.y + spriteDetails.height >= platOneDetails.y)
+   {return true}
+   else {return false}
+};
+
 
 const whichkey = (logkey) => { 
     if (logkey.code === 'ArrowUp' && parseInt(sprite.style.bottom)<=420){
@@ -38,7 +50,7 @@ const whichkey = (logkey) => {
    } else {}
    };
 const gravityEffects = () => { 
-   if (parseInt(sprite.style.bottom)>= 1) {
+   if (parseInt(sprite.style.bottom)>= 1 && collisionDetect() === false) {
       let heightString = sprite.style.bottom.replace('px', '')
       var height = parseInt(heightString, 10)
       sprite.style.bottom = `${height - 1}px`
